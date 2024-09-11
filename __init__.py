49e5eeff668af34de533f7e88d6081eabe248b1b
SyntycheMpouEkouya7 <syntyche.glory@gmail.com>
2024-09-11 11:29:17 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -33,36 +33,40 @@ def monhistogramme():
 
 @app.route('/commits/')
 def display_commits():
-    # Afficher la page HTML qui contient le graphique
-    return render_template('commits.html')  # Assurez-vous d'avoir un fichier `commits.html` dans le dossier `templates`
+    # Affiche la page HTML qui contient le graphique
+    return render_template('commits.html')  # Assurez-vous que ce fichier est dans le dossier 'templates'
 
 @app.route('/commits-data')
 def get_commit_data():
-    # URL de l'API pour récupérer les commits du repository
+    # URL de l'API GitHub pour récupérer les commits
     api_url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
-    
+
     try:
+        # Effectue une requête GET pour récupérer les données de l'API
         response = requests.get(api_url)
         response.raise_for_status()  # Vérifie si la requête a réussi
 
+        # Transforme les données en format JSON
         commits = response.json()
         # Extraire les minutes de chaque commit
         minutes_list = [extract_minutes(commit['commit']['author']['date']) for commit in commits]
 
-        # Compter les occurrences de chaque minute
+        # Compte le nombre de commits pour chaque minute de 0 à 59
         minute_counts = {minute: minutes_list.count(minute) for minute in range(60)}
 
+        # Retourne les données sous format JSON pour être utilisé par le graphique
         return jsonify(minute_counts)
     except requests.exceptions.RequestException as e:
+        # Gestion des erreurs de la requête
         return jsonify({'error': str(e)}), 500
 
 def extract_minutes(date_string):
     """
-    Extrait les minutes d'une date au format ISO.
+    Extrait les minutes d'une date au format ISO : '2024-02-11T11:57:27Z'
     """
     date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
     return date_object.minute
-                                                                                                                                       
+  
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
